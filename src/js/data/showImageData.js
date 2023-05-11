617994ab2fc432d7d3530dfeea2bf1b24f605f04
ZeInacioCB <zeinaciocb@gmail.com>
2023-05-11 16:17:22 +0100

    assets inside public

--- src/js/data/showImageData.js
+++ src/js/data/showImageData.js
@@ -2,28 +2,28 @@
 export const images = [
     {
         id: 1,
-        url: '../../assets/images/carinaGaspar_show_1.png',
+        url: 'public/assets/images/carinaGaspar_show_1.png',
         title: "Carina Gaspar Show 1",
         description: "Carina Gaspar Show 1",
         alt: "Carina Gaspar Show 1"
     },
     {
         id: 2,
-        url: '../../assets/images/carinaGaspar_show_2.png',
+        url: 'public/assets/images/carinaGaspar_show_2.png',
         title: "Carina Gaspar Show 2",
         description: "Carina Gaspar Show 2",
         alt: "Carina Gaspar Show 2"
     },
     {
         id: 3,
-        url: '../../assets/images/carinaGaspar_show_3.png',
+        url: 'public/assets/images/carinaGaspar_show_3.png',
         title: "Carina Gaspar Show 3",
         description: "Carina Gaspar Show 3",
         alt: "Carina Gaspar Show 3"
     },
     {
         id: 4,
-        url: '../../assets/images/carinaGaspar_show_4.png',
+        url: 'public/assets/images/carinaGaspar_show_3.png',
         title: "Carina Gaspar Show 4",
         description: "Carina Gaspar Show 4",
         alt: "Carina Gaspar Show 4"
