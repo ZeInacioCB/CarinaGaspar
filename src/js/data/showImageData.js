688af37400db311bb51e4ac6e59a99ccf9b89558
ZeInacioCB <zeinaciocb@gmail.com>
2023-05-11 16:03:54 +0100

    fix relative paths

--- src/js/data/showImageData.js
+++ src/js/data/showImageData.js
@@ -1,33 +1,29 @@
-import carina_show1_url from './../../assets/images/carinaGaspar_show_1.png';
-import carina_show2_url from './../../assets/images/carinaGaspar_show_2.png';
-import carina_show3_url from './../../assets/images/carinaGaspar_show_3.png';
-import carina_show4_url from './../../assets/images/carinaGaspar_show_4.png';
 
 export const images = [
     {
         id: 1,
-        url: carina_show1_url,
+        url: '../../assets/images/carinaGaspar_show_1.png',
         title: "Carina Gaspar Show 1",
         description: "Carina Gaspar Show 1",
         alt: "Carina Gaspar Show 1"
     },
     {
         id: 2,
-        url: carina_show2_url,
+        url: '../../assets/images/carinaGaspar_show_2.png',
         title: "Carina Gaspar Show 2",
         description: "Carina Gaspar Show 2",
         alt: "Carina Gaspar Show 2"
     },
     {
         id: 3,
-        url: carina_show3_url,
+        url: '../../assets/images/carinaGaspar_show_3.png',
         title: "Carina Gaspar Show 3",
         description: "Carina Gaspar Show 3",
         alt: "Carina Gaspar Show 3"
     },
     {
         id: 4,
-        url: carina_show4_url,
+        url: '../../assets/images/carinaGaspar_show_4.png',
         title: "Carina Gaspar Show 4",
         description: "Carina Gaspar Show 4",
         alt: "Carina Gaspar Show 4"
