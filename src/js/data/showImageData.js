f200c63849b0946445b1f4ee84f8ffab0a118ed8
ZeInacioCB <zeinaciocb@gmail.com>
2023-05-11 16:20:34 +0100

    import images for bundling

--- src/js/data/showImageData.js
+++ src/js/data/showImageData.js
@@ -1,29 +1,34 @@
+import carinaGaspar_show_1 from '../../../public/assets/images/carinaGaspar_show_1.png';
+import carinaGaspar_show_2 from '../../../public/assets/images/carinaGaspar_show_2.png';
+import carinaGaspar_show_3 from '../../../public/assets/images/carinaGaspar_show_3.png';
+import carinaGaspar_show_4 from '../../../public/assets/images/carinaGaspar_show_4.png';
+
 
 export const images = [
     {
         id: 1,
-        url: 'public/assets/images/carinaGaspar_show_1.png',
+        url: carinaGaspar_show_1,
         title: "Carina Gaspar Show 1",
         description: "Carina Gaspar Show 1",
         alt: "Carina Gaspar Show 1"
     },
     {
         id: 2,
-        url: 'public/assets/images/carinaGaspar_show_2.png',
+        url: carinaGaspar_show_2,
         title: "Carina Gaspar Show 2",
         description: "Carina Gaspar Show 2",
         alt: "Carina Gaspar Show 2"
     },
     {
         id: 3,
-        url: 'public/assets/images/carinaGaspar_show_3.png',
+        url: carinaGaspar_show_3,
         title: "Carina Gaspar Show 3",
         description: "Carina Gaspar Show 3",
         alt: "Carina Gaspar Show 3"
     },
     {
         id: 4,
-        url: 'public/assets/images/carinaGaspar_show_3.png',
+        url: carinaGaspar_show_4,
         title: "Carina Gaspar Show 4",
         description: "Carina Gaspar Show 4",
         alt: "Carina Gaspar Show 4"
